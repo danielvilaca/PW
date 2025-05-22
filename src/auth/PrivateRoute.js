706222906aa763b6feeb0dacec202d953b4b7666
Marco Alves <a23036@alunos.ipca.pt>
2025-05-22 01:24:01 +0100

    Navbar

--- src/auth/PrivateRoute.js
+++ src/auth/PrivateRoute.js
@@ -4,9 +4,6 @@ import { Navigate } from 'react-router-dom';
 const PrivateRoute = ({ children }) => {
   const { user } = useAuth();
 
-  if (user === null) {
-    return <div>Loading...</div>; // Spinner
-  }
 
   return user ? children : <Navigate to="/login" />;
 };
